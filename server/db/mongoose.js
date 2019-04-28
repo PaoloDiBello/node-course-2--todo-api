3d37da7a337300ebd6f26df1c8adbce5741ea062
PaoloDiBello <dibellopol@gmail.com>
2019-04-28 15:34:19 +0200

    Add GET /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,3 +8,4 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 module.exports = {
     mongoose
 }
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./model/todo');
 const { User } = require('./model/user');
 
+const { ObjectID } = require('mongodb')
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -24,7 +26,6 @@ app.post('/todos', (req, res) => {
     // console.log(req.body);
 });
 
-// GET todos/ObjectID
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
@@ -33,6 +34,30 @@ app.get('/todos', (req, res) => {
         })
     }, (e) => {
         res.status(400).send(e)
+    })
+});
+
+// GET todos/ObjectID
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({})
+        return console.log('Id is invalid');
+    }
+
+    console.log('id', id)
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send({})
+            return console.log('Todo not found');
+        }
+
+        res.send({ todo })
+    }, (error) => {
+        console.log('error: ', error)
+        res.status(400).send({})
+
     })
 });
 
