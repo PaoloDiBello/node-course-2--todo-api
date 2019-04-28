1959bec1058d94c458333f2e09d3ec367689492f
PaoloDiBello <dibellopol@gmail.com>
2019-04-28 22:02:00 +0200

    Add PUT todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./model/todo');
 const { User } = require('./model/user');
@@ -73,6 +74,28 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({ todo })
     }).catch(e => res.status(400).send())
 
+});
+
+app.put('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({})
+    }
+    const body = _.pick(req.body, ['text', 'completed'])
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+        res.send({ todo })
+    }).catch(e => { res.status(400).send() })
 });
 
 
